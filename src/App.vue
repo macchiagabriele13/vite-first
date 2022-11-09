5173d637aa5f53e07b71e3e1c27ed84637a235d8
macchiagabriele13 <macchia_gabriele@libero.it>
2022-11-09 15:37:14 +0100

    Commit iniziale - Creazione App

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+
+// #1 importo qui eventuali componenti
+
+import { AppTitle } from "./components/AppTitle.vue";
+
+
+// #2 definisco una proprietá components per registrare i componenti importati al punto 1
+export default {
+
+    components: {
+        AppTitle
+    }
+
+}
+
+</script>
+
+<template>
+
+    <apptitle></apptitle>
+
+</template>
+
+<style>
+
+</style>
